44e1733582e7735c81e9fb28503877c71ef18852
safi222 <safia2000gibril@gmail.com>
2024-07-02 06:26:17 +0000

    solution

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,8 @@
+reate a file in the tmp directory called school
+file { '/tmp/school':
+  ensure  => file,
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet'
+}
---
+++ 0x0A-configuration_management/README.md
@@ -0,0 +1,1 @@
+solution for 0x0A-configuration_management project
