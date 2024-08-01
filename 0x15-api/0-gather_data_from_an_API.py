2f4b0a0418812ef03e65e0837ce4e89c27f5efad
safi222 <safia2000gibril@gmail.com>
2024-08-01 03:29:29 -0700

    loads

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python3
-
-
-
 """
 A script that returns information
 about TODO list progress for an employee
@@ -13,7 +10,6 @@ Expected output:
     (NUMBER_OF_DONE_TASKS/TOTAL_NUMBER_OF_TASKS):
     Second and N next lines display the title of completed tasks:
     TASK_TITLE (with 1 tabulation and 1 space before the TASK_TITLE)
-
 """
 
 if __name__ == "__main__":
@@ -23,7 +19,7 @@ if __name__ == "__main__":
 
     th_url = f'https://jsonplaceholder.typicode.com/users/{argv[1]}'
     with urlopen(th_url) as th_data:
-        name = loads(th_data.read())['name']
+        emplo_name = loads(th_data.read())['name']
     with urlopen(f'{th_url}/todos') as todos:
         todo_list = loads(todos.read())
         num_tasks = 0
@@ -34,5 +30,6 @@ if __name__ == "__main__":
                 completed += 1
                 completed_list.append(task['title'])
             num_tasks += 1
-        print(f'Employee {name} is done with tasks({completed}/{num_tasks}):')
+
+        print(f'Employee {emplo_name} is done with tasks({completed}/{num_tasks}):')
         [print(f'\t {title}') for title in completed_list]
